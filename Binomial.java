3f1cb813f7f49d70afe9785d8271bc5ecfeec87a
נטע כנרי <netakanary@MacBook-Pro-sl-nt.local>
2026-01-01 09:50:17 +0200

    fix binomal

--- Binomial.java
+++ Binomial.java
@@ -22,8 +22,8 @@ public class Binomial {
 	}
 
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		int[][] memo = new int[n + 1][k + 1];
+	public static long binomial(int n, int k) {
+		long[][] memo = new long[n + 1][k + 1];
 
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= k; j++) {
@@ -34,20 +34,21 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
-		// Base case
-		if ((k > n)) {
+
+		if (k > n) {
 			memo[n][k] = 0;
 			return 0;
 		}
-		// Another base case
-		if (n == 0 || k == 0) {
+
+		if (k == 0 || n == 0) {
 			memo[n][k] = 1;
 			return 1;
 		}
+
 		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
 		return memo[n][k];
 	}
